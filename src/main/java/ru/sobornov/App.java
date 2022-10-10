6b8bc485a8c5545b1d47cfbf27ba7ebb7deb6656
Vladimir Sobornov <thecodegod1@gmail.com>
2022-10-10 16:23:36 +0300

    Initial commit

---
+++ src/main/java/ru/sobornov/App.java
@@ -0,0 +1,30 @@
+package ru.sobornov;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Configuration configuration = new Configuration();
+
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+
+        try {
+            session.beginTransaction();
+
+            session.getTransaction().commit();
+
+        } finally {
+            sessionFactory.close();
+
+        }
+    }
+}
